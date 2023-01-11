abe447ea6d26924e3e65e8aa38990e32d8d4a19c
Nguyen Duan <87711300+DuanNguyen2907@users.noreply.github.com>
2023-01-11 12:48:10 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Sidebar />
-      <Layout/>
+      <div className="App">
+        <div></div>
+        <Layout/>
 
       </div>
-    // </div>
+    </div>
   );
 }
 
