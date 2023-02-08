3f140073ca42a4c542ab78479312fc73bba8d932
Nguyen Duan <87711300+DuanNguyen2907@users.noreply.github.com>
2023-02-08 16:28:12 +0700

    update search and sort function

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,20 +1,75 @@
-import React from 'react'
+import React from "react";
 import ShipmentsTable from "../Table/Table";
 import SearchAria from "../SearchAria/SearchAria";
-import './Layout.css'
-import {Routes, Route} from 'react-router-dom'
+import "./Layout.css";
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 const Layout = () => {
+  const [shipments, setShipments] = useState([]);
+  const [result, setResult] = useState();
+  const [searchData, setSearchData] = useState("");
+  const [sortData, setSortData] = useState("");
+
+  const handleDataChange = (newData) => {
+    setSearchData(newData);
+  };
+
+  const handleSortDataChange = (newData) => {
+    setSortData(newData);
+  };
+
+  let url = "";
+  if (searchData === "" && sortData === "") {
+    url =
+      "https://delivery-module-production.up.railway.app/api/shipping_order";
+  } else if (searchData === "" && sortData === 6) {
+    url =
+      "https://delivery-module-production.up.railway.app/api/shipping_order";
+  } else if (searchData === "" && sortData !== 0 && sortData !== 6) {
+    url =
+      "https://delivery-module-production.up.railway.app/api/shipping_order/group/status/" +
+      sortData;
+  } else {
+    url =
+      "https://delivery-module-production.up.railway.app/api/shipping_order/" +
+      searchData;
+  }
+  console.log(url);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      // http://localhost:8083/api/shipping_order
+      // https://delivery-module-production.up.railway.app/api/shipping_order
+      .then((res) => {
+        console.log(res.data);
+        setShipments(res.data.data);
+        setResult(res.data.result.responseCode);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [url]);
+  console.log("sort:", typeof sortData);
   return (
     <div>
-    <SearchAria/>
+      <SearchAria onDataChange={handleDataChange} />
       <div className="Layout">
-        <Routes className='Main'>
-            <Route path='/' element=<ShipmentsTable/>/>
+        <Routes className="Main">
+          <Route
+            path="/"
+            element=<ShipmentsTable
+              shipments={shipments}
+              result={result}
+              onDataChange={handleSortDataChange}
+            />
+          />
         </Routes>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
