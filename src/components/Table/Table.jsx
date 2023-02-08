6f42d99ec530c84d4aa7972c62213fe0370685b1
DuanNguyen2907 <87711300+DuanNguyen2907@users.noreply.github.com>
2023-02-08 16:25:13 +0700

    update sort and search function

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -9,61 +9,91 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import DetailCard from "../DetailCard/DetailCard";
 import "./Table.css";
-import NativeSelect from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
+import NativeSelect from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
 import MainDash from "../MainDash/MainDash";
-import axios from "axios";
 
-const DateTime = (time) => {
-  var date = time.split("T");
-
-  return date[0];
-};
+// const DateTime = (time) => {
+//   var date = time.split("T");
+//   return date[0];
+// };
 
 const options = [
-  { id:1 ,value: 'chưa giao hàng', label: 'chưa giao hàng' },
-  { id:2 ,value: 'đang giao hàng', label: 'đang giao hàng' },
-  { id:3 ,value: 'đã giao hàng', label: 'đã giao hàng' },
-  { id:4 ,value: 'hủy giao hàng', label: 'hủy giao hàng' },
-  { id:5 ,value: 'trả hàng', label: 'trả hàng' }
+  { id: 1, value: "chưa giao hàng", label: "chưa giao hàng" },
+  { id: 2, value: "đang giao hàng", label: "đang giao hàng" },
+  { id: 3, value: "đã giao hàng", label: "đã giao hàng" },
+  { id: 4, value: "hủy giao hàng", label: "hủy giao hàng" },
+  { id: 5, value: "trả hàng", label: "trả hàng" },
 ];
 
-export default function ShipmentsTable() {
+const sortOptions = [
+  { id: 1, value: "chưa giao hàng", label: "chưa giao hàng" },
+  { id: 2, value: "đang giao hàng", label: "đang giao hàng" },
+  { id: 3, value: "đã giao hàng", label: "đã giao hàng" },
+  { id: 4, value: "hủy giao hàng", label: "hủy giao hàng" },
+  { id: 5, value: "trả hàng", label: "trả hàng" },
+  { id: 6, value: "toàn bộ", label: "trả hàng" },
+];
+
+export default function ShipmentsTable(props) {
+  let param = props.shipments;
+  let result = props.result;
+  let onChange = props.onDataChange;
   return (
     <div className="Shipments">
       <MainDash />
-      <BasicTable />
+      <BasicTable shipment={param} result={result} onChange={onChange} />
     </div>
   );
 }
 
-function BasicTable() {
-  const [shipments, setShipments] = useState([]);
+function BasicTable(props) {
   const [details, setDetails] = useState(false);
-  const [changedID,setChangedID] = useState();
-  const [cardDetail,setCardDetail] = useState({});
-  useEffect(() => {
-    axios
-      .get(
-        "https://delivery-module-production.up.railway.app/api/shipping_order"
-      )
-      // http://localhost:8083/api/shipping_order
-      // https://delivery-module-production.up.railway.app/api/shipping_order
-      .then((res) => {
-        setShipments(res.data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-  console.log("Shipments:", shipments);
+  const [changedID, setChangedID] = useState();
+  const [cardDetail, setCardDetail] = useState({});
+
+  const handleChange = (event) => {
+    onchange(event.target.value);
+  };
+  let shipment = props.shipment;
+  let result = props.result;
+  let onchange = props.onChange;
+  let shipments = [];
+  if (shipment !== undefined) {
+    if (shipment.length === undefined) {
+      shipments = [props.shipment];
+    } else {
+      shipments = props.shipment;
+    }
+  }
+  console.log("shipmetsresult:", shipments);
+  console.log("result2:", result);
   return (
     <div className="Table">
       <h2>ORDER LIST</h2>
       <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+              <TableCell align="left"></TableCell>
+              <TableCell align="left"></TableCell>
+              <TableCell align="left"></TableCell>
+              <TableCell align="left">
+                <NativeSelect
+                  value={sortOptions.value}
+                  id={sortOptions.id}
+                  options={sortOptions}
+                  defaultValue={sortOptions[5].id}
+                  onChange={handleChange}
+                >
+                  {sortOptions.map((item) => (
+                    <MenuItem value={item.id}>{item.value}</MenuItem>
+                  ))}
+                </NativeSelect>
+              </TableCell>
+            </TableRow>
             <TableRow>
               <TableCell>Mã Đơn</TableCell>
               <TableCell align="left">Ngày đặt đơn</TableCell>
@@ -81,7 +111,7 @@ function BasicTable() {
                 <TableCell component="th" scope="row">
                   {row.orderId}
                 </TableCell>
-                <TableCell align="left">{DateTime(row.createAt)}</TableCell>
+                <TableCell align="left">{row.createAt}</TableCell>
                 <TableCell align="left">{row.receiver.name}</TableCell>
                 <TableCell align="left">{row.fee}</TableCell>
                 <TableCell align="left">
@@ -90,18 +120,20 @@ function BasicTable() {
                     id={options.id}
                     options={options}
                     defaultValue={options[row.statusCode.id - 1].id}
-                    onChange={(event)=>{setChangedID(event.target.value)}}
+                    onChange={(event) => {
+                      setChangedID(event.target.value);
+                    }}
                   >
-                  {options.map((item)=>
-                    (<MenuItem value={item.id}>{item.value}</MenuItem>)
-                  )}
+                    {options.map((item) => (
+                      <MenuItem value={item.id}>{item.value}</MenuItem>
+                    ))}
                   </NativeSelect>
                 </TableCell>
                 <TableCell
                   align="left"
                   className="Details"
                   onClick={() => {
-                    debugger
+                    debugger;
                     setCardDetail(row);
                     setDetails(true);
                   }}
@@ -109,28 +141,38 @@ function BasicTable() {
                   details
                 </TableCell>
                 <TableCell>
-                  <input type="button" value="Update" onClick={()=> {
-                    let url = "https://delivery-module-production.up.railway.app/api/shipping_order/status/"+row.orderId+'/'+changedID
-                    console.log(url);
-                    fetch(url,
-                    {
-                      method:"PUT",
-                      headers: {
-                        'Content-Type': 'application/json',
-                        'Access-Control-Allow-Origin': '*'
-                      }
-                    })
-                  }
-                  }></input>
+                  <input
+                    type="button"
+                    value="Update"
+                    onClick={() => {
+                      let url =
+                        "https://delivery-module-production.up.railway.app/api/shipping_order/status/" +
+                        row.orderId +
+                        "/" +
+                        changedID;
+                      console.log(url);
+                      fetch(url, {
+                        method: "PUT",
+                        headers: {
+                          "Content-Type": "application/json",
+                          "Access-Control-Allow-Origin": "*",
+                        },
+                      }).then(() => {
+                        window.location.reload();
+                      });
+                    }}
+                  ></input>
                 </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
-      {
-        details ? (<DetailCard card={cardDetail} setDetail = {()=>setDetails(false)}/>):""
-      }
+      {details ? (
+        <DetailCard card={cardDetail} setDetail={() => setDetails(false)} />
+      ) : (
+        ""
+      )}
     </div>
   );
 }
