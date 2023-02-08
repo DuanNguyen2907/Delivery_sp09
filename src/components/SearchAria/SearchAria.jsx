3eab4ad63316a9948fdfe536093b21a06af76b55
Nguyen Duan <87711300+DuanNguyen2907@users.noreply.github.com>
2023-02-08 16:26:37 +0700

    update search function

--- src/components/SearchAria/SearchAria.jsx
+++ src/components/SearchAria/SearchAria.jsx
@@ -1,17 +1,33 @@
-import React from 'react'
-import './SearchAria.css'
+import React from "react";
+import "./SearchAria.css";
+import { useState } from "react";
 
-const SearchAria = () => {
+import { UilSearchAlt } from "@iconscout/react-unicons";
+
+const SearchAria = (props) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+  const handleClick = () => {
+    props.onDataChange(searchTerm);
+  };
   return (
-    <div className='SearchAria'>
-        <div className='Search'>
-            <input type='text' placeholder='Search'></input>
+    <div className="SearchAria">
+      <div className="Search">
+        <input
+          type="text"
+          value={searchTerm}
+          placeholder="Search"
+          onChange={handleChange}
+        ></input>
+        <div className="Search-Button">
+          <UilSearchAlt onClick={handleClick} />
         </div>
-        <div className='User'>
-            
-        </div>
+      </div>
+      <div className="User"></div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchAria
+export default SearchAria;
